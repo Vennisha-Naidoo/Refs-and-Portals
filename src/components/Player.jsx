256508f705aaba73b7361fccaccfccda6f5c07b9
Vennisha Naidoo <vennisha.naidoo@appstage.co.za>
2024-11-04 13:54:07 +0200

    Manipulating the DOM via Refs

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -5,21 +5,15 @@ export default function Player() {
   const playerName = useRef(); //JavaScript Object
 
   const [enteredPlayerName, setEnteredPlayerName] = useState('');
-  // const [submitted, setSubmitted] = useState(false);
-
-  // function handleChange(event) {
-  //   setSubmitted(false);
-  //   setEnteredPlayerName(event.target.value);
-  // }
 
   function handleClick() {
-    // setSubmitted(true);
     setEnteredPlayerName(playerName.current.value);
+    playerName.current.value = '';
   }
 
   return (
     <section id="player">
-      <h2>Welcome { /* submitted */ enteredPlayerName ?? 'unknown entity' }</h2>
+      <h2>Welcome { enteredPlayerName ?? 'unknown entity' }</h2>
       <p>
         <input 
         ref={ playerName } 
